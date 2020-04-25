35cf23b339b7cb98d517c02d81189aad98bfff06
Villy Devlioti <vadevlio@gmail.com>
2020-04-25 19:12:18 -0400

    header style created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Header from "./Components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*
+import ElectionHome from "./components/ElectionHome/ElectionHome";
+import Quiz from "./components/Quiz/Quiz";
+import Login from "./components/Login/Login";
+import Mainpage from "./components/Mainpage/Mainpage";
+import Signup from "./components/Signup/Signup";
+import pageData from "./pageData"; */
+
+class App extends Component {
+  render() {
+    return (
+      
+/*       <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" render={() => <Mainpage pageData={pageData}/>}/>
+            <Route exact path="/candidates" render={() => <Candidates pageData={pageData}/>}/>
+            <Route exact path="/electionhome/:id" render={() => <ElectionHome pageData={pageData}/>}/>
+            <Route exact path="/quiz" render={() => <Quiz pageData={pageData}/>}/>
+            <Route exact path="/login" render={() => <Login pageData={pageData}/>}/>
+            <Route exact path="/signup" render={() => <Signup pageData={pageData}/>}/>
+          </Switch>
+          </div>
+      </Router> */
+      <Header />
+      
+    )}}
 
 export default App;
---
+++ src/Components/TwitterCard/TwitterCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TwitterCard.css";
+
+
+const TwitterCard = () => (
+    <div class="card">
+  </div>
+);
+
+export default TwitterCard;
