5440c44ecc8c8efab5c221f4a5f5b06044927aa9
Villy Devlioti <vadevlio@gmail.com>
2020-05-07 19:55:11 -0400

    just another try

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
   //this is our connection to the back end!
   callTwitterApi = async () => {
-    await axios.get('https://raw.githubusercontent.com/VillyDevlioti/quarantine-life/api/tweets')
+    await axios.get('https://raw.githubusercontent.com/VillyDevlioti/quarantine-life/tweets')
       .then(res => {
           //pipeline length control
           if (this.state.count === 20){ 
